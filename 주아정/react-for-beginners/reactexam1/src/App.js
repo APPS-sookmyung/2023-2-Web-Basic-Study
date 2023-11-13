c6a68d27eeb6b0c4c7182c9eb78914d9cbe42154
ajeong <ajung7038@naver.com>
2023-11-13 18:33:35 +0900

    Feat: State 실습 코드 추가

--- 주아정/react-for-beginners/reactexam1/src/App.js
+++ 주아정/react-for-beginners/reactexam1/src/App.js
@@ -3,10 +3,12 @@
 import React from "react";
 
 import MyHeader from "./MyHeader";
+import Counter from "./Counter";
 
 function App() {
   let name = "주아정";
 
+  /*
   const style = {
     App: {
       backgroundColor: "black",
@@ -23,15 +25,14 @@ function App() {
     return "func";
   };
 
+  */
+
   const number = 5;
 
   return (
-    <div style={style.App}>
+    <div>
       <MyHeader />
-      <h2 style={style.h2}>안녕 리액트 {func()}</h2>
-      <b style={style.bold_text} id="bold_text">
-        {number}는 : {number % 2 === 0 ? "짝수" : "홀수"}
-      </b>
+      <Counter />
     </div>
   );
 }
