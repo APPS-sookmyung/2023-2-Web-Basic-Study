447fa4bc563a057b251a171d9b0ffb31955f5064
ajeong <ajung7038@naver.com>
2023-11-13 17:53:49 +0900

    Feat: React.Fragment 태그 실습

--- 주아정/react-for-beginners/reactexam1/src/App.js
+++ 주아정/react-for-beginners/reactexam1/src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 
+import React from "react";
+
+import MyHeader from "./MyHeader";
+
 function App() {
   let name = "주아정";
   return (
-    <div className="App">
+    <React.Fragment>
+      <MyHeader />
       <header className="App-header">
         <h2>안녕 리액트 {name}</h2>
       </header>
-    </div>
+    </React.Fragment>
   );
 }
 
--- 주아정/react-for-beginners/reactexam1/src/index.js
+++ 주아정/react-for-beginners/reactexam1/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// import MyFooter from "./MyFooter";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <MyFooter /> */}
   </React.StrictMode>
 );
 
