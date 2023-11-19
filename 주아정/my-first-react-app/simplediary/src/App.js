e7d491c8c921f62e9000974554364af2fa99de7d
ajeong <ajung7038@naver.com>
2023-11-19 21:57:47 +0900

    Feat: 리스트 렌더링 (map)

--- 주아정/my-first-react-app/simplediary/src/App.js
+++ 주아정/my-first-react-app/simplediary/src/App.js
@@ -1,10 +1,43 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "이정환",
+    content: "하이 1",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "이정환",
+    content: "하이 2",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "주아정",
+    content: "하이 13",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: "이정환",
+    content: "하이 14",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <DiaryEditor></DiaryEditor>
+      <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
---
+++ 주아정/my-first-react-app/simplediary/src/DiaryList.js
@@ -0,0 +1,26 @@
+const DiaryList = ({ diaryList }) => {
+  console.log(diaryList);
+  return (
+    <div className="DiaryList">
+      <h2>일기 리스트</h2>
+      <h4>{diaryList.length} 개의 일기가 있습니다</h4>
+      <div>
+        {/* div 3개가 있는 배열 */}
+        {diaryList.map((it) => (
+          <div>
+            <div>작성자 : {it.author} </div>
+            <div>일기 : {it.content} </div>
+            <div>감정 : {it.emotion} </div>
+            <div>작성 시간(ms) : {it.content} </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+DiaryList.defaultProps = {
+  diaryList: [],
+};
+
+export default DiaryList;
