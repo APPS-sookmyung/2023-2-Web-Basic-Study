0b9da6cba80fbf82aace5aa33b191e391602e803
ajeong <ajung7038@naver.com>
2023-11-21 01:30:10 +0900

    Feat: React 배열 사용 - 데이터 추가

--- 주아정/my-first-react-app/simplediary/src/App.js
+++ 주아정/my-first-react-app/simplediary/src/App.js
@@ -2,42 +2,29 @@ import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-const dummyList = [
-  {
-    id: 1,
-    author: "이정환",
-    content: "하이 1",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "이정환",
-    content: "하이 2",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "주아정",
-    content: "하이 13",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 4,
-    author: "이정환",
-    content: "하이 14",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-];
+import { useRef, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]); // 일기 데이터를 전달 -> 배열로 초깃값 설정
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
--- 주아정/my-first-react-app/simplediary/src/DiaryList.js
+++ 주아정/my-first-react-app/simplediary/src/DiaryList.js
@@ -7,21 +7,7 @@ const DiaryList = ({ diaryList }) => {
       <h4>{diaryList.length} 개의 일기가 있습니다</h4>
       <div>
         {diaryList.map((it) => (
-          <DiaryItem key={it.id} {...it} /> // 이게 문제다
-          // <DiaryItem
-          //   key={it.id}
-          //   author={it.author}
-          //   content={it.content}
-          //   created_date={it.created_date}
-          //   emotion={it.emotion}
-          //   id={it.id}
-          // />
-          // <div key={it.id}>
-          //   <div>작성자 : {it.author} </div>
-          //   <div>일기 : {it.content} </div>
-          //   <div>감정 : {it.emotion} </div>
-          //   <div>작성 시간(ms) : {it.content} </div>
-          // </div>
+          <DiaryItem key={it.id} {...it} />
         ))}
       </div>
     </div>
