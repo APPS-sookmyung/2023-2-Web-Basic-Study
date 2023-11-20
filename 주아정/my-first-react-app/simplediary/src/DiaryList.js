2932a5c40342cc29df727ce33a6230ff47c95d85
ajeong <ajung7038@naver.com>
2023-11-20 12:40:28 +0900

    Feat: 리스트 렌더링 컴포넌트 분리

--- 주아정/my-first-react-app/simplediary/src/DiaryList.js
+++ 주아정/my-first-react-app/simplediary/src/DiaryList.js
@@ -1,18 +1,27 @@
+import DiaryItem from "./DiaryItem";
+
 const DiaryList = ({ diaryList }) => {
-  console.log(diaryList);
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
       <h4>{diaryList.length} 개의 일기가 있습니다</h4>
       <div>
-        {/* div 3개가 있는 배열 */}
         {diaryList.map((it) => (
-          <div>
-            <div>작성자 : {it.author} </div>
-            <div>일기 : {it.content} </div>
-            <div>감정 : {it.emotion} </div>
-            <div>작성 시간(ms) : {it.content} </div>
-          </div>
+          <DiaryItem key={it.id} {...it} /> // 이게 문제다
+          // <DiaryItem
+          //   key={it.id}
+          //   author={it.author}
+          //   content={it.content}
+          //   created_date={it.created_date}
+          //   emotion={it.emotion}
+          //   id={it.id}
+          // />
+          // <div key={it.id}>
+          //   <div>작성자 : {it.author} </div>
+          //   <div>일기 : {it.content} </div>
+          //   <div>감정 : {it.emotion} </div>
+          //   <div>작성 시간(ms) : {it.content} </div>
+          // </div>
         ))}
       </div>
     </div>
